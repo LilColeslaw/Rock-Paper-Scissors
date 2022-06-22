c33b51ee609385d3a92d14fcc34860e6a41f51a7
Winslow Solomon <winsolo11@gmail.com>
2022-06-21 20:16:58 -0400

    Make playRound() method to be used later
    Make a playRound() method to simulate one round of the game using
    two arguments - the player's selection and the computer's selection.
    It returns a string saying the result of the game.

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function computerPlay() {
     // make it an int from 0 to 2 and use that int to access one of the array items and return it
     return options[Math.floor(Math.random() * 3)];
 }
+
+function playRound(computerSelection, playerSelection) {
+    //check if they equal each-other return a tie if they do
+    if (computerSelection === playerSelection) {
+        return `It's a tie! ${computerSelection} ties ${playerSelection}!`;
+    }
+    //check the cases for the computer having Rock
+    if (computerSelection === "Rock") {
+        if (playerSelection === "Scissors") {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        } 
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    //check the cases for the computer having Paper
+    if (computerSelection === "Paper") {
+        if (playerSelection === "Rock") {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    //check the cases for the computer having Scissors
+    if (playerSelection === "Paper") {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    if (playerSelection === "Rock") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+}
