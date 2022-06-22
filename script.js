318dc0a973c8b637ec12196b1ecdc02d7817cff4
Winslow Solomon <winsolo11@gmail.com>
2022-06-21 22:06:59 -0400

    Make game() function and change
    Made a game function which makes a five round game for the
    user to play when it is called. It prompts the user for a move
    each round and shows stats for each previous game and also shows
    overall stats. At the end of the five rounds it declares if you win,
    lose, or tie.
    
    In order to do this the playRound() function had to be reworked
    so that instead of returning strings it returned numbers which
    would correspond to a win, loss, tie, or non-valid move. This needed
    to be done because the game() function wouldn't be able to
    collect stats from the old playRound() function. The game() function
    could use numbers to collect stats and also send the messages that
    the playRound() function was going to. If the user enters a non-valid
    move, the function will alert them of this and not count the round
    as one of the five.

--- script.js
+++ script.js
@@ -6,39 +6,165 @@ function computerPlay() {
     return options[Math.floor(Math.random() * 3)];
 }
 
-function playRound(computerSelection, playerSelection) {
+function playRound(computerSelection, playerSelection) { //make it so that 0 = loss, 1 = tie, 2 = win, 3 = non-valid move
     //take the playerSelection argument and make sure it has the right capitalization
     let pSLength = playerSelection.length; //make a variable for playerSelection's length
     playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1, pSLength).toLowerCase();
     //check if they equal each-other return a tie if they do
     if (computerSelection === playerSelection) {
-        return `It's a tie! ${computerSelection} ties ${playerSelection}!`;
+        return 1;
     }
     //check the cases for the computer having Rock
     if (computerSelection === "Rock") {
         if (playerSelection === "Scissors") {
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
+            return 0;
         } 
         if (playerSelection === "Paper") {
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+            return 2;
         }
     }
     //check the cases for the computer having Paper
     if (computerSelection === "Paper") {
         if (playerSelection === "Rock") {
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
+            return 0;
         }
         if (playerSelection === "Scissors") {
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+            return 2;
         }
     }
     //check the cases for the computer having Scissors
     if (playerSelection === "Paper") {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        return 0;
     }
     if (playerSelection === "Rock") {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        return 2;
     }
     //if none of the other cases are true, tell the user that they didn't enter a valid move
-    return "You have not entered a valid move";
+    return 3;
+}
+
+function game() {
+    //simulates a 5-round game using a for-loop
+
+    //create a variable for the result of each round
+    let roundOne = "Round 1: ";
+    let roundTwo = "Round 2: ";
+    let roundThree = "Round 3: ";
+    let roundFour = "Round 4: ";
+    let roundFive = "Round 5: ";
+
+
+    //create variables for total wins and losses and ties
+    let wins = 0;
+    let losses = 0;
+    let ties = 0;
+
+    //create a for loop
+    //initialize with the variable games which will be set to 0
+    //keep it going until it is greater than or equal to 5 (the rounds)
+    //each time add 1 to it
+    for (let rounds = 0; rounds < 5; rounds++) {
+        //send a message prompting the user for a move and store it in a variable
+        let playerSelection = prompt("Enter a move: ");
+
+        //call computerPlay() and store it in a variable
+        let computerSelection = computerPlay();
+
+        //call playRound() using ^ these two variables and store the result in a variable
+        let result = playRound(computerSelection, playerSelection);
+        //find out if result is a win or a loss or a tie and log corresponding message and do action
+            //add it to the corresponding round variable
+            //increase wins or losses or ties variable
+        switch (result) {
+            case 0:
+                console.log(`YOU LOSE! ${computerSelection} beats ${playerSelection}`);
+                losses++;
+                switch (rounds) {
+                    case 0:
+                        roundOne += `Loss - ${playerSelection} vs ${computerSelection}`;
+                        break;
+                    case 1:
+                        roundTwo += `Loss - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n`);
+                        break;
+                    case 2:
+                        roundThree += `Loss - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n${roundTwo}\n`);
+                        break;
+                    case 3:
+                        roundFour += `Loss - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n${roundTwo}\n${roundThree}\n`);
+                        break;
+                    case 4:
+                        roundFive += `Loss - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n${roundTwo}\n${roundThree}\n${roundFour}\n`);
+                        break;
+                }
+                break;
+            case 1:
+                console.log(`IT'S A TIE! ${computerSelection} ties ${playerSelection}!`);
+                ties++;
+                switch (rounds) {
+                    case 0:
+                        roundOne += `Tie - ${playerSelection} vs ${computerSelection}`;
+                        break;
+                    case 1:
+                        roundTwo += `Tie - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n`);
+                        break;
+                    case 2:
+                        roundThree += `Tie - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n${roundTwo}\n`);
+                        break;
+                    case 3:
+                        roundFour += `Tie - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n${roundTwo}\n${roundThree}\n`);
+                        break;
+                    case 4:
+                        roundFive += `Tie - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n${roundTwo}\n${roundThree}\n${roundFour}\n`);
+                        break;
+                }
+                break;
+            case 2: 
+                console.log(`YOU WIN! ${playerSelection} beats ${computerSelection}`);
+                wins++;
+                switch (rounds) {
+                    case 0:
+                        roundOne += `Win - ${playerSelection} vs ${computerSelection}`;
+                        break;
+                    case 1:
+                        roundTwo += `Win - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n`);
+                        break;
+                    case 2:
+                        roundThree += `Win - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n${roundTwo}\n`);
+                        break;
+                    case 3:
+                        roundFour += `Win - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n${roundTwo}\n${roundThree}\n`);
+                        break;
+                    case 4:
+                        roundFive += `Win - ${playerSelection} vs ${computerSelection}`;
+                        console.log(`Stats:\n${roundOne}\n${roundTwo}\n${roundThree}\n${roundFour}\n`);
+                        break;
+                }
+                break;
+            case 3:
+                console.log("You have not entered a valid move");
+                rounds--;
+                break;
+        }
+        //print out the total wins/losses/ties
+        console.log(`Total Wins: ${wins}\nTotal Losses: ${losses}\nTotal Ties: ${ties}\n`);
+    }
+    //print out the winner based on if you have more wins or losses - if equal it's a tie
+    if (wins > losses) {
+        console.log("You win the game!");
+    } else if (losses > wins) {
+        console.log("You lose the game!");
+    } else {
+        console.log("You tie with the computer!");
+    }
 }
