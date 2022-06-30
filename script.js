e648d1e698a5881f514e836bb73178362b0a5e7e
Winslow Solomon <winsolo11@gmail.com>
2022-06-29 21:00:08 -0400

    Finish basic new UI version
    Made three buttons - one for each possible move - and determined
    game results based on those buttons. Two divs were made - one for
    storing the current score, and one for messages about the results
    of rounds and games. Many changes were made to playRound() and game()
    was deleted.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let wins = 0;
+let losses = 0;
+
+const resultArea = document.querySelector(".result");
+
+
+const rock = document.querySelector(".Rock");
+rock.addEventListener("click", playRound);
+
+const paper = document.querySelector(".Paper");
+paper.addEventListener("click", playRound);
+
+const scissors = document.querySelector(".Scissors");
+scissors.addEventListener("click", playRound);
+
+const score = document.querySelector(".score");
+score.textContent = `Current score: ${wins} vs ${losses}`;
+
 function computerPlay() {
     //create an array which holds the strings "Rock", "Paper", and "Scissors"
     const options = ["Rock", "Paper", "Scissors"];
@@ -6,76 +24,72 @@ function computerPlay() {
     return options[Math.floor(Math.random() * 3)];
 }
 
-function playRound(computerSelection, playerSelection) { //make it so that 0 = loss, 1 = tie, 2 = win, 3 = non-valid move
-    //take the playerSelection argument and make sure it has the right capitalization
-    let pSLength = playerSelection.length; //make a variable for playerSelection's length
-    playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1, pSLength).toLowerCase();
-    //check if they equal each-other return a tie if they do
-    if (computerSelection === playerSelection) {
-        return 1;
-    }
-    //check the cases for the computer having Rock
-    if (computerSelection === "Rock") {
-        if (playerSelection === "Scissors") {
-            return 0;
-        } 
-        if (playerSelection === "Paper") {
-            return 2;
+function playRound(event) {
+    let computerSelection = computerPlay();
+    let playerSelection = event.target.getAttribute("class");
+    if (wins < 4 && losses < 4) {
+        //check if they equal each-other return a tie if they do
+        if (computerSelection === playerSelection) {
+            resultArea.textContent = "IT WAS A TIE. RE-ENTER A MOVE";
+            return;
         }
-    }
-    //check the cases for the computer having Paper
-    if (computerSelection === "Paper") {
-        if (playerSelection === "Rock") {
-            return 0;
-        }
-        if (playerSelection === "Scissors") {
-            return 2;
-        }
-    }
-    //check the cases for the computer having Scissors
-    if (playerSelection === "Paper") {
-        return 0;
-    }
-    if (playerSelection === "Rock") {
-        return 2;
-    }
-    //if none of the other cases are true, tell the user that they didn't enter a valid move
-    return 3;
-}
-
-function game() {
-    //create variables for total wins and losses and ties
-    let wins = 0;
-    let losses = 0;
-
-    while (wins < 5 && losses < 5) { //checks if either player has over 5 points
-        //send a message prompting the user for a move and store it in a variable
-        let playerSelection = prompt("Enter a move: ");
-
-        //call computerPlay() and store it in a variable
-        let computerSelection = computerPlay();
-
-        //call playRound() using ^ these two variables and store the result in a variable
-        let result = playRound(computerSelection, playerSelection);
-        //find out if result is a win or a loss or a tie and log corresponding message and do action
-            //add it to the corresponding round variable
-            //increase wins or losses or ties variable
-        switch (result) {
-            case 0:
-                console.log(`YOU LOSE! ${computerSelection} beats ${playerSelection}`);
+        //check the cases for the computer having Rock
+        if (computerSelection === "Rock") {
+            if (playerSelection === "Scissors") {
+                resultArea.textContent = `YOU LOSE! ${computerSelection} beats ${playerSelection}`;
                 losses++;
-                break;
-            case 1:
-                console.log(`IT WAS A TIE. RE-ENTER MOVE`);
-                break;
-            case 2: 
-                console.log(`YOU WIN! ${playerSelection} beats ${computerSelection}`);
+            } else if (playerSelection === "Paper") {
+                resultArea.textContent = `YOU WIN! ${playerSelection} beats ${computerSelection}`;
                 wins++;
-                break;
+            }
+        } else if (computerSelection === "Paper") {
+            if (playerSelection === "Rock") {
+                resultArea.textContent = `YOU LOSE! ${computerSelection} beats ${playerSelection}`;
+                losses++;
+            } else if (playerSelection === "Scissors") {
+                resultArea.textContent = `YOU WIN! ${playerSelection} beats ${computerSelection}`;
+                wins++;
+            }
+        } else if (playerSelection === "Paper") {
+            resultArea.textContent = `YOU LOSE! ${computerSelection} beats ${playerSelection}`;
+            losses++;
+        } else if (playerSelection === "Rock") {
+            resultArea.textContent = `YOU WIN! ${playerSelection} beats ${computerSelection}`;
+            wins++;
         }
-        //print out the total wins/losses/ties
-        console.log(`Your points: ${wins}\n The computer's points: ${losses}`);
+        score.textContent = `Current score: ${wins} vs ${losses}`;
+    } else if (wins === 4 || losses === 4) {
+        //check if they equal each-other return a tie if they do
+        if (computerSelection === playerSelection) {
+            resultArea.textContent = "IT WAS A TIE. RE-ENTER A MOVE";
+            return;
+        }
+        //check the cases for the computer having Rock
+        if (computerSelection === "Rock") {
+            if (playerSelection === "Scissors") {
+                losses++;
+            } 
+            if (playerSelection === "Paper") {
+                wins++;
+            }
+        } else if (computerSelection === "Paper") {
+            if (playerSelection === "Rock") {
+                losses++;
+            }
+            if (playerSelection === "Scissors") {
+                wins++;
+            }
+        } else if (playerSelection === "Paper") {
+            losses++;
+        } else if (playerSelection === "Rock") {
+            wins++;
+        }
+        score.textContent = `Current score: ${wins} vs ${losses}`;
+        if (wins === 5 || losses === 5) {
+            let result;
+            wins === 5 ? result = "WIN" : result = "LOSE";
+            resultArea.textContent = `YOU ${result} THE GAME, WITH ${wins} POINTS`;
+            return;
+        }
     }
-    //print out the winner based on if you have more wins or losses
-    wins > losses ? console.log("You win!") : console.log("You lose.");
 }
