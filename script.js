ea2ac139fd9885eec77cb5a3652a33157f302999
Winslow Solomon <winsolo11@gmail.com>
2022-06-30 17:42:19 -0400

    Increase smartness of javascript and add images
    New functions were made in the javascript to decrease the repetition
    of statements. An array and forEach method were added to use less code
    for adding the eventListeners to the rock, paper, and scissors elements.
    
    Background images were added to the "move selection" elements and the
    text was removed. The elements were put in a flexbox display to create
    a better looking page. eventListeners were added to detect the motion
    of a mouse over the elements, and a function was run in these listeners
    which changed the background color.

--- script.js
+++ script.js
@@ -7,18 +7,23 @@ const resultArea = document.querySelector(".result");
 
 
 const rock = document.querySelector(".Rock");
-rock.addEventListener("click", playRound);
 
 const paper = document.querySelector(".Paper");
-paper.addEventListener("click", playRound);
 
 const scissors = document.querySelector(".Scissors");
-scissors.addEventListener("click", playRound);
+let movesArray = [rock, paper, scissors];
+movesArray.forEach((move) => {
+    move.addEventListener("click", playRound);
+    move.addEventListener("mouseover", mousedOver);
+    move.addEventListener("mouseleave", mouseLeft);
+});
 
 const score = document.querySelector(".score");
 score.textContent = `Current score: ${wins} vs ${losses}`;
 
-const changeContent = (element, num) => {
+function mousedOver(event) {event.target.style.backgroundColor = "#65c5df";}
+function mouseLeft(event) {event.target.style.backgroundColor = "white";}
+function changeContent (element, num) {
     switch (num) {
         case 0:
             element.textContent = `YOU LOSE! ${playerSelection} loses to ${computerSelection}`;
@@ -31,9 +36,8 @@ const changeContent = (element, num) => {
             break;
         default:
             element.textContent = `Current score: ${wins} vs ${losses}`;
-            break;
     }
-};
+}
 
 function computerPlay() {
     //create an array which holds the strings "Rock", "Paper", and "Scissors"
@@ -86,21 +90,27 @@ function playRound(event) {
         //check the cases for the computer having Rock
         if (computerSelection === "Rock") {
             if (playerSelection === "Scissors") {
+                changeContent(resultArea, 0);
                 losses++;
             } 
             if (playerSelection === "Paper") {
+                changeContent(resultArea, 1);
                 wins++;
             }
         } else if (computerSelection === "Paper") {
             if (playerSelection === "Rock") {
+                changeContent(resultArea, 0);
                 losses++;
             }
             if (playerSelection === "Scissors") {
+                changeContent(resultArea, 1);
                 wins++;
             }
         } else if (playerSelection === "Paper") {
+            changeContent(resultArea, 0);
             losses++;
         } else if (playerSelection === "Rock") {
+            changeContent(resultArea, 1);
             wins++;
         }
         changeContent(score, 3);
